205227c689fb8c1afedb0e99d3f3622a4cf15def
Oways Asi <owayslfkf@gmail.com>
2024-04-09 14:12:48 +0300

    added mockup data

---
+++ src/data/users.js
@@ -0,0 +1,7 @@
+export const users = [
+  { Id: 412322, name: "Jake Phil", email: "a@.co", phone: "00588562" },
+  { Id: 467567, name: "Ali khalid", email: "we@.com", phone: "00534562" },
+  { Id: 567543, name: "Mo Salim", email: "mo@.co", phone: "00233872" },
+  { Id: 346878, name: "Tom Dell", email: "tom@.co", phone: "01296062" },
+  { Id: 976466, name: "Sarah Jad", email: "sara@.co", phone: "00586762" },
+];
